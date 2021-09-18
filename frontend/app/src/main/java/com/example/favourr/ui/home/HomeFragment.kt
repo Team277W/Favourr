620d1b5496b1daf5cc8618f322f6720f150b6e67
jmfan2002 <jmfan2002@gmail.com>
2021-09-18 12:49:35 -0400

    bt connection

--- frontend/app/src/main/java/com/example/favourr/ui/home/HomeFragment.kt
+++ frontend/app/src/main/java/com/example/favourr/ui/home/HomeFragment.kt
@@ -4,55 +4,30 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
-import com.example.favourr.R
-import com.google.android.gms.nearby.connection.*
+import com.example.favourr.MainViewModel
+import com.example.favourr.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
 
     private lateinit var homeViewModel: HomeViewModel
-    private lateinit var connectionsClient: ConnectionsClient
+    private val mainViewModel: MainViewModel by activityViewModels()
+    private var _binding: FragmentHomeBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
-            inflater: LayoutInflater,
-            container: ViewGroup?,
-            savedInstanceState: Bundle?
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
     ): View? {
-        homeViewModel =
-                ViewModelProvider(this).get(HomeViewModel::class.java)
-        val root = inflater.inflate(R.layout.fragment_home, container, false)
-        val textView: TextView = root.findViewById(R.id.text_home)
-        homeViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
+        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        homeViewModel = ViewModelProvider(this).get(HomeViewModel::class.java)
+        mainViewModel.connectionId.observe(viewLifecycleOwner, Observer {
+            binding.textHome.text = it
         })
-        return root
-    }
-
-    private fun startAdvertising() {
-        val localName = "TestUsername"
-
-        val advertisingOptions = AdvertisingOptions.Builder()
-            .setStrategy(Strategy.P2P_STAR)
-    }
-
-    companion object {
-        private const val SERVICE_ID = "com.team277.favourr.SERVICE_ID"
-        private val connectionLifecycleCallback = object : ConnectionLifecycleCallback() {
-            override fun onConnectionInitiated(p0: String, p1: ConnectionInfo) {
-                TODO("Not yet implemented")
-            }
-
-            override fun onConnectionResult(p0: String, p1: ConnectionResolution) {
-                TODO("Not yet implemented")
-            }
-
-            override fun onDisconnected(p0: String) {
-                TODO("Not yet implemented")
-            }
-
-        }
+        return binding.root
     }
 }
