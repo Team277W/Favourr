bc64a5dec0ad43fce760eb2d129c259c6b3bb95a
jmfan2002 <jmfan2002@gmail.com>
2021-09-18 23:15:06 -0400

    profile fragment

---
+++ frontend/app/src/main/java/com/example/favourr/ui/connections/ProfileFragment.kt
@@ -0,0 +1,30 @@
+package com.example.favourr.ui.connections
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.example.favourr.databinding.FragmentProfileBinding
+
+class ProfileFragment : Fragment() {
+
+    private lateinit var profileViewModel: ProfileViewModel
+    private lateinit var binding: FragmentProfileBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        profileViewModel = ViewModelProvider(this).get(ProfileViewModel::class.java)
+        binding = FragmentProfileBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        // TODO: Network
+    }
+}
---
+++ frontend/app/src/main/java/com/example/favourr/ui/connections/ProfileViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.favourr.ui.connections
+
+import androidx.lifecycle.ViewModel
+
+class ProfileViewModel : ViewModel() {
+
+}
